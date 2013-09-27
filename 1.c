43f4e6a7292f823819fe504c13b229498e29e825
Asheem Chhetri <achhetri@live.com>
2013-09-27 16:25:14 -0500

    Changes made to learna nd added 1.c

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int x = 10;
+	int y = 29;
+	unsigned int z = x+y;
+	printf("The sum is:%d\n",z);
+	return 0;
+}
--- learn.c
+++ learn.c
@@ -3,5 +3,6 @@
 int main()
 {
 	printf("Hello, World!");
+	printf("Hello Asheem!");	
 	return 0;
 }
